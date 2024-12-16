1e80d3862de0e5113c290c7395290e357d8008e6
Oscar Armando Luna <oscar@oscarluna.dev>
2024-12-15 16:07:55 -0800

    Adding Google Authentication

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,12 @@
 export const environment = {
   firebase: {
+   
     apiKey: "AIzaSyCTk9slGFt53ASUiFyK3dmkn08j3RAuCqM",
     authDomain: "friendlychat-8f093.firebaseapp.com",
     projectId: "friendlychat-8f093",
     storageBucket: "friendlychat-8f093.firebasestorage.app",
     messagingSenderId: "59325276303",
     appId: "1:59325276303:web:73cd8174941ad1d43150fa",
-    measurementId: "G-X27HD97DSY"
   },
 };
+
